0eb6913daef6a977488d543ca3fc5bfca55ce4b2
Victoria Olko <victoriaolko@Victorias-MacBook-Air.local>
2026-05-01 00:25:16 -0400

    Fix iOS auto-zoom on input focus

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import { AuthProvider } from '../lib/AuthContext';
+
 export default function App({ Component, pageProps }) {
-  return <AuthProvider><Component {...pageProps}/></AuthProvider>;
+  return (
+    <AuthProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps}/>
+    </AuthProvider>
+  );
 }
